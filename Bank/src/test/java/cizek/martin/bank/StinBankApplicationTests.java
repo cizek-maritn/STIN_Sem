2d2e753df7c37ddc8b472a2353e07512c3889e7b
Dell <martin.cizek@tul.cz>
2023-04-25 20:22:27 +0200

    init commit

---
+++ Bank/src/test/java/cizek/martin/bank/StinBankApplicationTests.java
@@ -0,0 +1,13 @@
+package cizek.martin.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StinBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
