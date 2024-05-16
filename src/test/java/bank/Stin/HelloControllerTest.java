75073cd64fdd9ae82fbb7b81334e33b87052b82d
Martin Čížek <officialpatera6@gmail.com>
2024-05-16 21:35:50 +0200

    hello world init (#1)

---
+++ src/test/java/bank/Stin/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package bank.Stin;
+
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+    
+        @Autowired
+        private MockMvc mvc;
+
+	@Test
+	void getHello() throws Exception {
+            mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo("Hello World!")));
+	}
+
+}
