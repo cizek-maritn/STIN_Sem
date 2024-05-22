8c9d839ffa4eeda4810a565a510c59eb56570dbc
Dell <martin.cizek@tul.cz>
2024-05-22 19:53:39 +0200

    curl fix attempt 3

--- src/main/java/bank/Stin/ApiCaller.java
+++ src/main/java/bank/Stin/ApiCaller.java
@@ -14,7 +14,7 @@ public class ApiCaller {
     public static int[] CallApiCurrent(String lat, String lon, String days) {
         String apiCall=CallBuilder(lat,lon,days);
         String[] command = {
-            "/bin/curl",
+            "curl",
             "-X", "GET",
             apiCall, "--ssl-no-revoke"
         };
